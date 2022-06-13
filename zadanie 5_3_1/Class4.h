ec9bf1befa67a955d8f6a2de97337c84aac0552f
nastasyas0 <107112915+nastasyas0@users.noreply.github.com>
2022-06-13 18:06:08 +0300

    Create Class4.h

---
+++ zadanie 5_3_1/Class4.h
@@ -0,0 +1,11 @@
+#ifndef __CLASS4_H
+#define __CLASS4_H
+#include "Class3.h"
+
+class Cl_4 : public Cl_3 {
+public:
+  Cl_4(int a1, int a2, int a3, int a4);
+  virtual int Calc(int x);
+};
+
+#endif
